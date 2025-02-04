b4f339e01902d6e020d12cf28b40ac54cb74d65f
lynnyaung <james342506@gmail.com>
2025-02-04 20:03:02 +0630

    second commit

--- lib/views/matchWidget.dart
+++ lib/views/matchWidget.dart
@@ -24,96 +24,93 @@ class MatchWidget extends StatelessWidget {
 
     return Container(
       child: Center(
-        child: Padding(
-          padding: EdgeInsets.only(right: 10),
-          child: Card(
-            color: Colors.cyan,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                SizedBox(
-                  width: 76,
-                  height: 80,
-                  child: Center(
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        teamName1,
-                        style: TextStyle(fontWeight: FontWeight.w200),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(width: 80, height: 80, child: Image.asset(teamImg1)),
-                // Text(now as String),
-                SizedBox(
-                  width: 80,
-                  child: SingleChildScrollView(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        StreamBuilder<DocumentSnapshot>(
-                            stream: ApiService().getGoalWeek(matchId),
-                            builder: (context, snapshot) {
-                              if (snapshot.connectionState ==
-                                  ConnectionState.waiting) {
-                                return CircularProgressIndicator(); // Show loading indicator
-                              }
-
-                              if (!snapshot.hasData ||
-                                  snapshot.data == null ||
-                                  !snapshot.data!.exists) {
-                                return Text("No data"); // Handle null case
-                              }
-                              var data =
-                                  snapshot.data!.data() as Map<String, dynamic>;
-
-                              // var data = snapshot.data!.data() as Map<String, dynamic>;
-                              int team1Score = data['team1Score'] ?? 0;
-                              int team2Score = data['team2Score'] ?? 0;
-
-                              // Display goal scores
-                              return Column(
-                                children: [
-                                  data["text"] == "VS"
-                                      ? Text(data["time"])
-                                      : Text(
-                                          '$team1Score - $team2Score',
-                                          style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 18,
-                                          ),
-                                        ),
-                                  SizedBox(
-                                    child: TextButton(
-                                        onPressed: () {},
-                                        child: Text("VitaMax")),
-                                  )
-                                ],
-                              );
-                            })
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: 76,
-                  height: 80,
-                  child: Image.asset(
-                    teamImg2,
-                  ),
-                ),
-                SizedBox(
-                  width: 100,
-                  child: Center(
-                      child: TextButton(
+        child: Card(
+          color: Colors.cyan,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(
+                width: 76,
+                height: 80,
+                child: Center(
+                  child: TextButton(
                     onPressed: () {},
-                    child: Text(teamName2,
-                        style: TextStyle(fontWeight: FontWeight.w200)),
-                  )),
-                )
-              ],
-            ),
+                    child: Text(
+                      teamName1,
+                      style: TextStyle(fontWeight: FontWeight.w200),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(width: 80, height: 80, child: Image.asset(teamImg1)),
+              // Text(now as String),
+              SizedBox(
+                width: 80,
+                child: SingleChildScrollView(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      StreamBuilder<DocumentSnapshot>(
+                          stream: ApiService().getGoalWeek(matchId),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return CircularProgressIndicator(); // Show loading indicator
+                            }
+        
+                            if (!snapshot.hasData ||
+                                snapshot.data == null ||
+                                !snapshot.data!.exists) {
+                              return Text("No data"); // Handle null case
+                            }
+                            var data =
+                                snapshot.data!.data() as Map<String, dynamic>;
+        
+                            // var data = snapshot.data!.data() as Map<String, dynamic>;
+                            int team1Score = data['team1Score'] ?? 0;
+                            int team2Score = data['team2Score'] ?? 0;
+        
+                            // Display goal scores
+                            return Column(
+                              children: [
+                                data["text"] == "VS"
+                                    ? Text(data["time"])
+                                    : Text(
+                                        '$team1Score - $team2Score',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18,
+                                        ),
+                                      ),
+                                SizedBox(
+                                  child: TextButton(
+                                      onPressed: () {},
+                                      child: Text("VitaMax")),
+                                )
+                              ],
+                            );
+                          })
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 76,
+                height: 80,
+                child: Image.asset(
+                  teamImg2,
+                ),
+              ),
+              SizedBox(
+                width: 100,
+                child: Center(
+                    child: TextButton(
+                  onPressed: () {},
+                  child: Text(teamName2,
+                      style: TextStyle(fontWeight: FontWeight.w200)),
+                )),
+              ),SizedBox(width:10)
+            ],
           ),
         ),
       ),
